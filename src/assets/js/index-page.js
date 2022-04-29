e175dd21ac45b142d3cd5fabfe67462b3b12a50d
Andrew Love <love.andrew.j@gmail.com>
2022-04-29 11:23:37 +0100

    Add fullpage license key

--- src/assets/js/index-page.js
+++ src/assets/js/index-page.js
@@ -7,29 +7,28 @@ import fullpage from 'fullpage.js';
 
 // Initializing it
 var fullPageInstance = new fullpage('#sitefullpage', {
-	anchors:['home', 'profile', 'experience', 'skills', 'education', 'contact'],
+    anchors: ['home', 'profile', 'experience', 'skills', 'education', 'contact'],
     //License key
-    licenseKey: null,
+    licenseKey: 'QQOKK-LKRR6-JDMO6-MQB3I-ANXVI',
     //Navigation
-	navigation: true,
-	navigationPosition: 'left',
-	navigationTooltips: ['Home', 'Profile', 'Experience', 'Skills', 'Education', 'Contact'],
+    navigation: true,
+    navigationPosition: 'left',
+    navigationTooltips: ['Home', 'Profile', 'Experience', 'Skills', 'Education', 'Contact'],
     //Design
-    sectionsColor:['#524b59', '#FFFFFF', '#F5F7FA', '#FFFFFF', '#F5F7FA', '#3c3641'],
+    sectionsColor: ['#524b59', '#FFFFFF', '#F5F7FA', '#FFFFFF', '#F5F7FA', '#3c3641'],
     //Scrolling
-	css3: true,
-	scrollingSpeed: 700,
-	autoScrolling: false,
-	fitToSection: true,
-	fitToSectionDelay: 1000,
-	bigSectionsDestination: top,
-	scrollBar: false,
-	easing: 'easeInOutCubic',
-	easingcss3: 'ease',
-	// Responsive design
-	responsiveWidth: 900,
-	afterResponsive: function(isResponsive){
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: false,
+    fitToSection: true,
+    fitToSectionDelay: 1000,
+    bigSectionsDestination: top,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    // Responsive design
+    responsiveWidth: 900,
+    afterResponsive: function(isResponsive) {
 
-	}
+    }
 });
-
