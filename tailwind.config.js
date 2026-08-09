ec7953b0d3bc67a9d9e7d93079b09ae59e2a52f7
Andrew Love <love.andrew.j@gmail.com>
2026-08-09 17:36:47 +0100

    fix tailwind css missing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,17 @@
 module.exports = {
     content: [
+        "./index.html",
         "./_includes/**/*.{html,js}",
         "./_layouts/**/*.{html,js}",
-        "./_posts/**/*.{html,js}",
+        "./_posts/**/*.{html,md,markdown}",
+        "./src/**/*.{html,js}",
     ],
     theme: {
         screens: {
             'sm': '640px',
-            // => @media (min-width: 640px) { ... }
-
             'md': '768px',
-            // => @media (min-width: 768px) { ... }
-
             'lg': '1024px',
-            // => @media (min-width: 1024px) { ... }
-
             'xl': '1280px',
-            // => @media (min-width: 1280px) { ... }
         },
         extend: {
             colors: {
@@ -30,4 +25,4 @@ module.exports = {
     },
     variants: {},
     plugins: [],
-}
+};
